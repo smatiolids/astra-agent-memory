653a0df16ef76770e9b400dc6f5d0e66ba71e65f
Samuel Matioli <samuel.matioli@datastax.com>
2023-12-20 18:37:32 -0300

    Converting code to Ragstack & Astrapy

--- app.py
+++ app.py
@@ -2,45 +2,44 @@ import streamlit as st
 from dotenv import load_dotenv, find_dotenv
 import os
 import cassio
-from langchain.memory import CassandraChatMessageHistory
+from langchain.memory import AstraDBChatMessageHistory
 from langchain.memory import ConversationSummaryBufferMemory
 from langchain.llms import OpenAI
 from langchain.indexes.vectorstore import VectorStoreIndexWrapper
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores.cassandra import Cassandra
+from langchain.vectorstores import AstraDB
 from langchain.chains.retrieval_qa.base import RetrievalQA
-from langchain import PromptTemplate
+from langchain.prompts import PromptTemplate
 
 load_dotenv(find_dotenv(), override=True)
 
-cassio.init(token=os.environ["ASTRA_DB_APPLICATION_TOKEN"], database_id=os.environ["ASTRA_DB_ID"])
-
 # Globals
-cqlMode = 'astra_db'  # 'astra_db'/'local'
-session = cassio.config.resolve_session()
-keyspace = cassio.config.resolve_keyspace()
+
 memory_table_name = 'vs_investment_memory'
 kb_table_name = 'vs_investment_kb'
+
 llm = OpenAI(temperature=0.1)
 embedding_generator = OpenAIEmbeddings()
 
-CassVectorStore = Cassandra(
-    session= cassio.config.resolve_session(),
-    keyspace= 'demo',
-    table_name= kb_table_name,
-    embedding=embedding_generator
+AstraVectorStore = AstraDB(
+    embedding=embedding_generator,
+    collection_name=kb_table_name,
+    token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+    api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
 )
 
 index = VectorStoreIndexWrapper(
-    vectorstore=CassVectorStore
+    vectorstore=AstraVectorStore
 )
 
+
 def clear_memory(conversation_id):
-    message_history = CassandraChatMessageHistory(
+    message_history = AstraDBChatMessageHistory(
         session_id=conversation_id,
-        session=session,
-        keyspace=keyspace,
-        ttl_seconds=3600,
+        token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+        api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
+        # ttl_seconds=3600,
+        collection_name=memory_table_name
     )
 
     message_history.clear()
@@ -49,10 +48,10 @@ def clear_memory(conversation_id):
     del st.session_state['summary']
     return True
 
+
 def start_memory():
     load_memory(st.session_state["conv_id_input"])
     return True
-
 
 
 def get_answer(conversation_id, q):
@@ -72,12 +71,12 @@ def get_answer(conversation_id, q):
         template=prompt_template, input_variables=["summaries", "question"]
     )
 
-    message_history = CassandraChatMessageHistory(
+    message_history = AstraDBChatMessageHistory(
         session_id=conversation_id,
-        session= cassio.config.resolve_session(),
-        keyspace= cassio.config.resolve_keyspace(),
-        ttl_seconds=3600,
-        table_name=memory_table_name
+        token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+        api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
+        # ttl_seconds=3600,
+        collection_name=memory_table_name
     )
 
     memory = ConversationSummaryBufferMemory(
@@ -87,7 +86,7 @@ def get_answer(conversation_id, q):
         buffer=""
     )
 
-    # retrieverSim = CassVectorStore.as_retriever(
+    # retrieverSim = AstraVectorStore.as_retriever(
     #     search_type='similarity_score_threshold',
     #     search_kwargs={
     #         'k': 5,
@@ -96,14 +95,14 @@ def get_answer(conversation_id, q):
     #     },
     # )
 
-    retrieverSim = CassVectorStore.as_retriever(
+    retrieverSim = AstraVectorStore.as_retriever(
         search_type='similarity',
         search_kwargs={
             'k': 5,
             'filter': {"source": st.session_state.file}
         },
     )
-    
+
     # Create a "RetrievalQA" chain
     chainSim = RetrievalQA.from_chain_type(
         llm=llm,
@@ -121,22 +120,23 @@ def get_answer(conversation_id, q):
 
     st.session_state.messages = memory.chat_memory.messages
     st.session_state.summary = new_summary
-    
+
     # Run it and print results
     answer = chainSim.run(q)
 
     return answer
 
+
 def load_memory(conversation_id, file):
     st.session_state.conversation_id = conversation_id
     st.session_state.file = file
 
-    message_history = CassandraChatMessageHistory(
+    message_history = AstraDBChatMessageHistory(
         session_id=conversation_id,
-        session=session,
-        keyspace=keyspace,
-        ttl_seconds=3600,
-        table_name=memory_table_name
+        token=os.environ["ASTRA_DB_APPLICATION_TOKEN"],
+        api_endpoint=os.environ["ASTRA_DB_API_ENDPOINT"],
+        # ttl_seconds=3600,
+        collection_name=memory_table_name
     )
 
     memory = ConversationSummaryBufferMemory(
@@ -187,8 +187,9 @@ if __name__ == "__main__":
 
     if 'summary' in st.session_state:
         st.divider()
-        st.text_area(label=f"Summary for conversation id: {st.session_state.conversation_id}", value=st.session_state.summary, height=200)
-    
+        st.text_area(
+            label=f"Summary for conversation id: {st.session_state.conversation_id}", value=st.session_state.summary, height=200)
+
     if 'messages' in st.session_state:
         st.divider()
         st.text_area(label="Memory", value=format_messages(
